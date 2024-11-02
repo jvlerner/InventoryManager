ef167b349f04366ab03b990c2e601f6d87b95ac7
jvlerner <jvlerner@uolinc.com>
2024-11-02 08:24:46 -0300

    fix: url api frontend

--- basic-inventory-dashboard/src/app/config/api.ts
+++ basic-inventory-dashboard/src/app/config/api.ts
@@ -3,12 +3,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "process.env.NEXT_PUBLIC_API_URL",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 15000,
   timeoutErrorMessage: "Timeout of basic-inventory-api, time waiting: 15 seconds"
 });
-
-console.log(process.env.NEXT_PUBLIC_API_URL)
 
 api.interceptors.request.use((request) => {
   console.log("Starting Request", request);
