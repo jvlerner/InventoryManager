cbc0050782fc0f24f20617afd3d369bbef5f65d5
João Victor <jvlerner@uolinc.com>
2024-11-11 05:29:57 -0300

    finished project

--- basic-inventory-dashboard/src/service/api.ts
+++ basic-inventory-dashboard/src/service/api.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api:8080", 
+  baseURL: "http://localhost:3002", 
   timeout: 15000,
   timeoutErrorMessage: "Timeout of basic-inventory-api, time waiting: 15 seconds"
 });
